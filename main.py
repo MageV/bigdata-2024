0f3339940e309e7c36bdd3f283be15e5a63fd558
master <morgoth2006@gmail.com>
2024-01-23 10:06:14 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import time
+
+
+def cache(func):
+    cache = {}
+
+    def inner(*args):
+        if (args in cache):
+            return cache[args]
+        else:
+            result = func(*args)
+            cache[args] = result
+            return result
+
+    return inner
+
+
+@cache
+def heavy_func(a, b):
+    time.sleep(5)
+    return a + b
+
+print (heavy_func(5,5))
+print (heavy_func(6,5))
+print (heavy_func(5,5))
