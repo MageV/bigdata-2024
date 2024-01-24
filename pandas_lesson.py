30fc87d85200c41a1ac8f154422a7dd76ce1671d
master <morgoth2006@gmail.com>
2024-01-24 10:45:15 +0300

    Initial commit

---
+++ pandas_lesson.py
@@ -0,0 +1,19 @@
+import random
+from datetime import datetime
+
+import matplotlib
+import pandas as pd
+from matplotlib import pyplot as plt
+import sys
+import numpy as np
+
+fmt = '%d-%m-%Y %H:%M:%S'
+start=datetime.strptime('24-01-2023 10:00:00',fmt)
+end=datetime.strptime('24-07-2023 10:00:00',fmt)
+dates=[random.random() * (end - start) + start for _ in range(10)]
+customers_count=[random.randint(0,100) for _ in range(10)]
+status=[random.randint(0,1) for _ in range(10)]
+dataset=list(zip(dates,customers_count,status))
+print(dataset)
+df=pd.DataFrame(data=dataset,columns=['date','customers_count','status']);
+print(df)
