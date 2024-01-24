451c5a3c4bb977fbd3c6316059560a929fbb7c5a
master <morgoth2006@gmail.com>
2024-01-24 11:36:19 +0300

    xlsx io ops

--- pandas_lesson.py
+++ pandas_lesson.py
@@ -10,7 +10,7 @@ def generate(rows_count=1):
     countries_pool = ['RU', 'KZ', 'US', 'CY', 'UA', 'AE']
     status_pool = [1, 2, 3]
     dates_rng = pd.date_range(start='1/24/2023', end='7/24/2023')
-    dates = [dates_rng[random.randint(0, dates_rng.values.size)] for _ in range(rows_count)]
+    dates = [dates_rng[random.randint(0, dates_rng.values.size-1)] for _ in range(rows_count)]
     customers_count = np.random.randint(low=25, high=1000, size=dates_rng.values.size)
     country = [countries_pool[random.randint(0, len(countries_pool) - 1)] for _ in range(rows_count)]
     status = [status_pool[random.randint(0, len(status_pool) - 1)] for _ in range(rows_count)]
@@ -23,3 +23,5 @@ def generate(rows_count=1):
 
 frame=generate(10)
 frame.to_excel('data/customers.xlsx',index=False,header=True,engine='openpyxl')
+frame2=pd.read_excel('data/customers.xlsx',index_col='date')
+print(frame2)
