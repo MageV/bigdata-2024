7bb1af9957ebfd997862b3ed268e46d40e894dfc
master <morgoth2006@gmail.com>
2024-01-24 11:01:06 +0300

    DataFrame from random seeds

--- pandas_lesson.py
+++ pandas_lesson.py
@@ -3,13 +3,20 @@ from datetime import datetime
 
 import pandas as pd
 
-fmt = '%d-%m-%Y %H:%M:%S'
-start = datetime.strptime('24-01-2023 10:00:00', fmt)
-end = datetime.strptime('24-07-2023 10:00:00', fmt)
-dates = [random.random() * (end - start) + start for _ in range(10)]
-customers_count = [random.randint(0, 100) for _ in range(10)]
-status = [random.randint(0, 1) for _ in range(10)]
-dataset = list(zip(dates, customers_count, status))
-print(dataset)
-df = pd.DataFrame(data=dataset, columns=['date', 'customers_count', 'status'])
-print(df)
+def generate(rows_count=1):
+    fmt = '%d-%m-%Y %H:%M:%S'
+    countries_pool=['RU','KZ','US','CY','UA','AE']
+    status_pool=[1,2,3]
+    start = datetime.strptime('24-01-2023 10:00:00', fmt)
+    end = datetime.strptime('24-07-2023 10:00:00', fmt)
+    dates = [random.random() * (end - start) + start for _ in range(rows_count)]
+    customers_count = [random.randint(0, 100) for _ in range(rows_count)]
+    country=[countries_pool[random.randint(0, len(countries_pool)-1)] for _ in range(rows_count)]
+    status = [status_pool[random.randint(0, len(status_pool)-1)] for _ in range(rows_count)]
+    dataset = list(zip(dates, customers_count,country, status))
+    print(dataset)
+    df = pd.DataFrame(data=dataset, columns=['date', 'customers_count','country', 'status'])
+    print(df)
+
+
+generate(10)
